5c1ba1964994b6aebeacac645971b908a9d82bc9
mohammed5000 <mohammed5000@github.com>
2026-02-11 16:11:42 +0000

    Test run: 1/3 passed, 2 failed, 0 skipped - 2026-02-11T16:11:42.023Z

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -17,6 +17,7 @@ public class Player {
     public String playerName;
     public int level;
 
+
     /**
      * this.playerName should be set to playerName.
      *
@@ -31,6 +32,7 @@ public class Player {
      * @param playerName the name of the player to be created.
      */
     public Player(String playerName){
+        this.playerName = playerName;
 
     }
 
@@ -48,6 +50,8 @@ public class Player {
      * @param level the starting level of the player to be created.
      */
     public Player(String playerName, int level){
+        this.playerName = playerName;
+        this.level = level;
 
     }
 
